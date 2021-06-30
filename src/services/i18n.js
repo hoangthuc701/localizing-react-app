408738ce7ae623da4d50f2a1be96a25a0ebdda9b
Hoang Thuc <hoangthuc701@gmail.com>
2021-06-30 22:31:26 +0700

    add auto detect user languge

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -1,11 +1,12 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpApi from "i18next-http-backend";
-
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18next
   .use(initReactI18next)
   .use(HttpApi) // Registering the back-end plugin
+  .use(LanguageDetector)
   .init({
     // Remove resources from here
     lng: "en",
