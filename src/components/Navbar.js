46b13b4f3150d9b6471ce31bd71853825cfb73d8
Hoang Thuc <hoangthuc701@gmail.com>
2021-06-30 22:29:29 +0700

    add multi language

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,32 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import logo from "../logo.png";
+import LanguageSwitcher from "./LanguageSwitcher";
 
-const Navbar = () => (
-  <nav className="navbar" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <a className="navbar-item" href="/">
-        <img className="navbar-logo" src={logo} alt="logo" />
-        <strong>My store</strong>
-      </a>
-    </div>
-    <div className="navbar-menu">
-      <div className="navbar-start">
+const Navbar = () => {
+  const { t } = useTranslation();
+
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
         <a className="navbar-item" href="/">
-          Weekly Basket
+          <img className="navbar-logo" src={logo} alt="logo" />
+          <strong>{t("app_name")}</strong>
         </a>
       </div>
-    </div>
-  </nav>
-);
-
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item" href="/">
+            Weekly Basket
+          </a>
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <LanguageSwitcher />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
